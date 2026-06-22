f8140d8ae0d961e96dc7e22622f1b0f3e52dcc03
Jess Riley Valentine <jesswaldroup282@gmail.com>
2026-06-22 01:07:35 -0700

    traffic data read in added

--- src/data/loaders.py
+++ src/data/loaders.py
@@ -6,3 +6,9 @@ def load_csv(filepath):
     Load a CSV file into a pandas DataFrame.
     """
     return pd.read_csv(filepath)
+
+def load_bike_counts(filepath):
+    """
+    Load Seattle bike counter data.
+    """
+    return pd.read_csv(filepath)
